ed219486ddf5ad5ef34c3db9c55eb3d766775ad2
Silvia Chen <silviachen@Silvias-MacBook-Pro.local>
2021-03-21 14:10:37 -0700

    Starts the Cooking App JSX - left portion

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
-import React, { useState } from 'react'
-import Counter from './Counter'
-import CounterHooks from './CounterHooks'
-
-export const ThemeContext = React.createContext()
+import React from 'react'
+import RecipeList from './RecipeList'
 
 function App() {
-  console.log("Render App")
-  const [theme, setTheme] = useState('green')
-  return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter
-      <Counter initialCount={0} />
-      CounterHooks
-      <CounterHooks initialCount={0} />
-      <button onClick={() => setTheme(prevTheme => 
-        prevTheme === 'green' ? 'blue' : 'green'
-      )}>Toggle Theme</button>
-    </ThemeContext.Provider>
-  )
+ return (
+   <RecipeList recipes={sampleRecipes}/>
+ )
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: '1. Put salt on Chiecken\n 2. Put chicken in oven\n 3. Eat chiecken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        ampunt: '2 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        ampunt: '1 Tbs'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: '1. Put paprika on pork\n 2. Put pork in oven\n 3. Eat pork',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        ampunt: '3 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        ampunt: '2 Tbs'
+      }
+    ]
+  }
+]
 
 export default App;
